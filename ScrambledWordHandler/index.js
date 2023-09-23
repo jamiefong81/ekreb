149e9d36aa2f57f93fcfcf4d4b6b543d53a15e79
jamie fong <jamiefong81@gmail.com>
2023-09-22 20:38:28 -0500

    Removed debugging lines

--- ScrambledWordHandler/index.js
+++ ScrambledWordHandler/index.js
@@ -10,13 +10,11 @@ app.use(bodyParser.text())
 let unscrambledWord;
 app.get("/data", (req,res) => {
     let [scrambled, unscrambled] = wordToScramble();
-    // var wordToSend = wordToScramble();
     unscrambledWord = unscrambled;
     res.send(scrambled);
 });
 
 app.post("/data", (req,res) => {
-    console.log('Got body: ', req.body);
     let correctOrNot = checkAnswer(req.body, unscrambledWord);
     res.status(200).send(`${correctOrNot.toString()},${unscrambledWord}`);
 });
